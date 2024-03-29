0df887665fc00fa3de84c2985f202aa8a5aebeb6
TLOWAC <leechanghun55@gmail.com>
2024-03-30 01:42:01 +0900

    feat: auth & user 모듈 등록

--- backend/nest-prisma-api/src/app.module.ts
+++ backend/nest-prisma-api/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SharedProviderModule } from './providers/shared-provider.module';
+import { AuthModule } from './domain/auth/auth.module';
+import { UserModule } from './domain/user/user.module';
 
 @Module({
-        imports: [SharedProviderModule],
+        imports: [SharedProviderModule, AuthModule, UserModule],
         controllers: [AppController],
         providers: [AppService],
 })
